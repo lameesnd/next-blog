d4375d9ea62f9319b9d0704f46eae21f199ebe7b
lamees-nd <lameesn@equitick.com>
2024-07-16 15:35:24 +0300

    disable eslint for spacing

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="px-6 mx-auto">
       <p className="my-12 text-3xl text-center dark:text-white">
+        {/* eslint-disable-next-line react/no-unescaped-entities  */}
         Hello and Welcome !! &nbsp;
         <span className="whitespace-nowrap">
           I'm <span className="font-bold">Lamees</span>
