06967321e3d802ae86bf026e203c1889fc9e272d
lamees-nd <lameesn@equitick.com>
2024-07-26 16:00:56 +0300

    add tailwindcss aspect-ratio, add Video & CustomImage components

--- lib/posts.ts
+++ lib/posts.ts
@@ -2,6 +2,8 @@ import { compileMDX } from 'next-mdx-remote/rsc'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeHighlight from 'rehype-highlight'
 import rehypeSlug from 'rehype-slug'
+import Video from '@/app/components/Video'
+import CustomImage from '@/app/components/CustomImage'
 
 type FileTree = {
   "tree": [
@@ -25,6 +27,9 @@ export async function getPostByName(fileName: string): Promise<BlogPost | undefi
 
   const { frontmatter, content } = await compileMDX<{ title: string, date: string, tags: string[] }>({
     source: rawMDX,
+    components: {
+      Video, CustomImage
+    },
     options: {
       parseFrontmatter: true,
       mdxOptions: {
